d4b2be8f03162cdaa122c3b0bb90eca29d58d422
Wine <newwine2048@gmail.com>
2020-02-25 20:19:12 +0800

    [Add] zsh theme p10k | gnome tweaks config

---
+++ gnome/tweaks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.wm.preferences titlebar-uses-system-font false
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'SauceCodePro NF Medium 16'
+gsettings set org.gnome.desktop.interface font-name 'SauceCodePro NF 13'
+gsettings set org.gnome.desktop.interface monospace-font-name 'SauceCodePro NF 13'
+gsettings set org.gnome.desktop.interface document-font-name 'SauceCodePro Nerd Font Medium 11'
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Flat-Remix-GTK-Green-Darkest-Solid-NoBorder'
+gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Green-Dark'
--- zsh/custom/custom.zsh
+++ zsh/custom/custom.zsh
@@ -1,5 +1,5 @@
 DEFAULT_USER="wine"
-ZSH_THEME="agnoster"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 dotfile="/home/wine/Documents/dotfiles/"
 
 alias vi="vim -X"
@@ -8,3 +8,12 @@ alias background="feh --bg-fill"
 
 #ros config
 source /opt/ros/melodic/setup.zsh
+
+# P10K
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+# instant prompt mode
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
