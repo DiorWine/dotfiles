8b38f2e240ca28227a4f8d080b0031f695f4e70d
wine <newwine2048@gmail.com>
2021-04-01 15:50:19 +0800

    [add] init dunst picom config

--- link.sh
+++ link.sh
@@ -3,16 +3,26 @@
 DOT_PATH=$(pwd)
 i3_PATH=$DOT_PATH/i3/
 compton_PATH=$DOT_PATH/compton/
+picom_PATH=$DOT_PATH/picom/
 rofi_PATH=$DOT_PATH/rofi/
+dunst_PATH=$DOT_PATH/dunst/
 
 
-rm -rf ~/.config/compton.conf
+# rm -rf ~/.config/compton.conf
+# cd ~/.config/
+# ln -s $compton_PATH/compton.conf .
+
+rm -rf ~/.config/picom.conf
 cd ~/.config/
-ln -s $compton_PATH/compton.conf .
+ln -s $picom_PATH/picom.conf .
 
 rm -rf ~/.config/rofi
 cd ~/.config/
 ln -s $rofi_PATH .
+
+rm -rf ~/.config/dunst
+cd ~/.config/
+ln -s $dunst_PATH .
 
 rm -rf ~/.config/i3
 cd ~/.config
