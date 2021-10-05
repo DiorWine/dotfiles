f9e7ef8421b7b10ee450e6ddd8ee75745ee17cb9
wine <newwine2048@gmail.com>
2021-10-05 13:41:40 +0800

    ver.1005

--- i3/startup/autopwr.sh
+++ i3/startup/autopwr.sh
@@ -2,6 +2,7 @@
 
 # Power Monitor
 # 低电量警告并黑屏，充电后恢复
+intern=eDP-1
 
 LOW=10
 WARN=5
@@ -12,7 +13,7 @@ do
     power_status=$(cat /sys/class/power_supply/BAT1/status)
 
     if [ $power_percent -lt $LOW ] && [ $power_status = "Discharging" ]; then
-        xrandr --output eDP-1-1 --brightness 0.4
+        xrandr --output $intern --brightness 0.4
         rofi -e "<span color='#DC322F'><b>电量低，请充电！</b></span>" -markup
         
         # 循环提醒
@@ -35,7 +36,7 @@ do
         done
 
         # 电量高于警戒线或插入电源，恢复亮度
-        xrandr --output eDP-1-1 --brightness 1
+        xrandr --output $intern --brightness 1
     fi
     sleep 1m
 done
