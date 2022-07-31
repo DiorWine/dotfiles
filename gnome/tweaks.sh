7f7a4f965c06aa7d18814dba8829df8f2fdbc0c7
Wine2048 <newwine2048@gmail.com>
2022-07-31 22:48:20 +0800

    [rofi gnome i3] new rofi config, gnome themes

--- gnome/tweaks.sh
+++ gnome/tweaks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.wm.preferences titlebar-uses-system-font false
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'SauceCodePro NF Medium 16'
-gsettings set org.gnome.desktop.interface font-name 'SauceCodePro NF 13'
-gsettings set org.gnome.desktop.interface monospace-font-name 'SauceCodePro NF 13'
-gsettings set org.gnome.desktop.interface document-font-name 'SauceCodePro Nerd Font Medium 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-uses-system-font true
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'SauceCodePro NF Bold 11'
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Flat-Remix-GTK-Green-Darkest-Solid-NoBorder'
-gsettings set org.gnome.desktop.interface icon-theme 'Flat-Remix-Green-Dark'
+gsettings set org.gnome.desktop.interface font-name 'SauceCodePro NF 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'SauceCodePro NF 13'
+gsettings set org.gnome.desktop.interface document-font-name 'SauceCodePro NF Medium 11'
+
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-DodgerBlue'
+gsettings set org.gnome.desktop.interface gtk-theme 'Flat-Remix-GTK-Violet-Darkest'
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru-magenta-dark'
